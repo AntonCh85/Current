2a753fbce7c5d58a409a56c7adf1f1a9c3a053c3
Anton <53342939+AntonCh85@users.noreply.github.com>
2020-07-08 14:39:23 +0200

    Current

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Fcomponent from './Fcomponent';
+import Ccomponent from './Ccomponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <Ccomponent name="Anton"/>
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code></code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
